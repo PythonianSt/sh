56aad7fd02c17d1538f4f05013fb3e0c311cf151
PythonianSt <152569656+PythonianSt@users.noreply.github.com>
2024-11-08 13:29:15 +0700

    Add files via upload

---
+++ set-trust-cert-2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo -n | openssl s_client -connect 10.104.11.155:9999 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' >server-cert.pem
+
+sudo keytool -delete -alias denodo-server -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit
+
+sudo keytool -import -alias denodo-server -file ./server-cert.pem -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit
